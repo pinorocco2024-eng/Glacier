432f468a8117159c11b8a10228c8618a8d73e930
pinorocco2024-eng <pinorocco2024@gmail.com>
2026-02-12 20:16:17 +0000

    Add

--- SplineScene.jsx
+++
@@ -1,7 +0,0 @@
-import Spline from '@splinetool/react-spline';
-
-export default function App() {
-  return (
-    <Spline scene="https://prod.spline.design/KEFCMhbmwF9b2lqY/scene.splinecode" />
-  );
-}
